03b0d4442d24f8ecbfff0ae6365738a80665c261
Jah-ziere <146846048+Jah-ziere@users.noreply.github.com>
2024-04-03 09:05:38 -0400

    Pipe movement and background movement

---
+++ Assets/scripts/MoveLeft.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    public float speed = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    { 
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+        // Moves pipes left
+        transform.Translate(Vector2.left * Time.deltaTime * speed);
+        
+    }
+}
